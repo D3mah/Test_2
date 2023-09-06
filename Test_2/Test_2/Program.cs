f9d727721554ba86df57ca4f21c41e66ca079fca
D3mahs <dima.kolmakov111@gmail.com>
2023-09-06 19:45:57 +0500

    #3 fix

--- Test_2/Test_2/Program.cs
+++ Test_2/Test_2/Program.cs
@@ -8,14 +8,49 @@ namespace Test_2
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             var i = 5;
-            var k = 5.2;
-            var l = false;
 
-            Console.WriteLine(i+k+l.ToInt32);
+
+            Console.WriteLine(i);
             Console.ReadLine();
+        }
+        /// <summary>
+        /// Класс, который представляет пользователя.
+        /// </summary>
+        public class User
+        {
+            private string name;
+
+            /// <summary>
+            /// Конструктор класса.
+            /// </summary>
+            /// <param name="name">Имя пользователя.</param>
+            public User(string name)
+            {
+                this.name = name;
+            }
+
+            /// <summary>
+            /// Меняет имя пользователя.
+            /// </summary>
+            /// <param name="name">Имя пользователя.</param>
+            public void ChangeName(string name)
+            {
+                this.name = name;
+            }
+
+            /// <summary>
+            /// Метод для вывода имени пользователя.
+            /// </summary>
+            /// <returns>Возвращает имя пользователя.</returns>
+            public string GetName()
+            {
+                return this.name;
+            }
+
         }
     }
 }
